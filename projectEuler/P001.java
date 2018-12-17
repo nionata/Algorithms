b9ddb389148e5a8515a5788c7582404d0423db2a
Nicholas Ionata <n.ionata129@gmail.com>
2018-12-16 22:59:30 -0500

    Create P001.java

---
+++ projectEuler/P001.java
@@ -0,0 +1,17 @@
+package me.nionata.euler;
+
+public class P001 {
+	
+	public static int run() {
+		int sum = 0;
+		
+		for(int i = 3; i < 1000; i++) {
+			if((i % 3) == 0 || (i % 5) == 0) {
+				sum += i;
+			}
+		}
+		
+		return sum;
+	}
+
+}
