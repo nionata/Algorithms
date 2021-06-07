c3ebdfb9a6af65707fc3e578238202c38b491e91
Nicholas Ionata <nionata@ufl.edu>
2021-06-06 22:30:13 -0700

    Update 1041. Robot Bounded In Circle.go

--- leetcode/1041. Robot Bounded In Circle.go
+++ leetcode/1041. Robot Bounded In Circle.go
@@ -1,3 +1,4 @@
+# O(n) / O(1)
 func isRobotBounded(instructions string) bool {
     x, y := 0, 0
     directions := [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
@@ -7,17 +8,9 @@ func isRobotBounded(instructions string) bool {
             x += directions[ptr][0]
             y += directions[ptr][1]
         } else if char == 'L' {
-            if ptr == 0 {
-                ptr = 3
-            } else {
-                ptr -= 1
-            }
+            ptr = (ptr + 3) % 4
         } else {
-            if ptr == 3 {
-                ptr = 0
-            } else {
-                ptr += 1
-            }
+            ptr = (ptr + 1) % 4
         }
     }
     return (x == 0 && y == 0) || ptr != 0
