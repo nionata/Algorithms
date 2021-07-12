c99c9b7e6ffd1ec6a111211b3e0b54cf387cab76
Nicholas Ionata <nionata@ufl.edu>
2021-07-11 21:44:55 -0700

    Update and rename 295. Find Median from Data Stream to 295. Find Median from Data Stream.py

--- leetcode/295. Find Median from Data Stream.py
+++ leetcode/295. Find Median from Data Stream.py
@@ -39,3 +39,34 @@ class MedianFinder:
     def isEven(self) -> bool:
 
         return len(self.maxHeap) == len(self.minHeap)
+        
+        
+class MedianFinder:
+
+    def __init__(self):
+        self.maxHeap = [] # n-2, n-1, n
+        self.minHeap = [] # n+1, n+2, n+3
+        
+    # O(log n)
+    def addNum(self, num: int) -> None:
+        if self.isEven():
+            # Get a new min and push to max
+            heappush(self.minHeap, num)
+            newMin = heappop(self.minHeap)
+            heappush(self.maxHeap, -newMin)
+        else:
+            # Get a new max and push to min
+            heappush(self.maxHeap, -num)
+            newMax = heappop(self.maxHeap)
+            heappush(self.minHeap, -newMax)
+
+    # O(1)
+    def findMedian(self) -> float:
+        if self.isEven():
+            return (self.minHeap[0] + (self.maxHeap[0]*-1))/2
+        else:
+            return self.maxHeap[0]*-1
+        
+    # O(1)
+    def isEven(self) -> bool:
+        return len(self.maxHeap) == len(self.minHeap)
