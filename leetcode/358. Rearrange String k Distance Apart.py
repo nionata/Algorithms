beb3fe4b21b9a87ba940746846b6c7f4bbaebc1e
Nicholas Ionata <nionata@ufl.edu>
2021-08-17 17:45:43 -0400

    Create 358. Rearrange String k Distance Apart.py

---
+++ leetcode/358. Rearrange String k Distance Apart.py
@@ -0,0 +1,19 @@
+# Time: O(nlogn)
+# Space: O(num uniques)
+def arrange(chars, min_sep):
+    counts, heap = Counter(chars), []
+    for char, count in counts:
+        heappush(heap, (-1 * count, char))
+    out = []
+    while len(heap):
+        elms = []
+        for _ in range(min_sep):
+            if len(heap) == 0:
+                break
+            elms.append(heappop(heap))
+        for count, char in elms:
+            out.append(char)
+            count+=1
+            if count < 0:
+                heappush(heap, (count, char))
+    return "".join(out) 
