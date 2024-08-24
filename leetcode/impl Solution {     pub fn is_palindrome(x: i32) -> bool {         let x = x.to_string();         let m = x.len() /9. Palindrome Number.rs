29fc5d7d0cff3c768cbe98ec8853ebf3bbd9fb4a
Nicholas Ionata <n.ionata129@gmail.com>
2024-08-24 10:26:54 -0700

    Delete leetcode/impl Solution {     pub fn is_palindrome(x: i32) -> bool {         let x = x.to_string();         let m = x.len()  directory

--- leetcode/impl Solution {     pub fn is_palindrome(x: i32) -> bool {         let x = x.to_string();         let m = x.len() /9. Palindrome Number.rs
+++
@@ -1,8 +0,0 @@
-impl Solution {
-    pub fn is_palindrome(x: i32) -> bool {
-        let x = x.to_string();
-        let m = x.len() / 2;
-
-        x.bytes().take(m).eq(x.bytes().rev().take(m))
-    }
-}
